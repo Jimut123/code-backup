da8ab532f00d302949915492b744d6b053026c5b
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2018-11-29 23:25:52 +0530

    pc

--- 8085_programming/YT/part_14_square_root.asm
+++ 8085_programming/YT/part_14_square_root.asm
@@ -1,17 +1,25 @@
-	   LXI H,2060
+// To find the square root of a perfect square
 	   MVI C,00
-	   MVI E,01
+	   MVI D,01
+	   LXI H,2060
+	   MOV A,M
+	   INR C
+	   SUB D
+	   JNZ L1
 
-L2:	   MOV A,M
-	   STC
-	   CMP E
-	   JNC L1
-	   MOV A,C
+L2:	   MOV A,C
 	   STA 2070
 	   HLT
 
-L1:	   MOV A,E
-	   ADI 02
-	   ADD E
+L1:	   INR D
+	   INR D
 	   INR C
+	   SUB D
+	   JNZ L1
 	   JMP L2
+// 1 -> 01H
+// 4 -> 04H
+// 9 -> 09H
+// 16 -> 10H
+# ORG 2060H
+# DB 10H
