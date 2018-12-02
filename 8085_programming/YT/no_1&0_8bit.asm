ddb81e32ae13127bbda8deaabfca9302bdfa1edb
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2018-12-02 23:54:16 +0530

    no of 0&1

---
+++ 8085_programming/YT/no_1&0_8bit.asm
@@ -0,0 +1,24 @@
+	   MVI C,00	// CONTAINS THE NUMBERS OF 0'S
+	   MVI B,00	// CONTAINS THE NUMBERS OF 1'S
+	   MVI E,08
+	   LXI H,2060
+	   MOV A,M
+
+L1:	   STC
+	   CMC
+	   RAL
+	   JC L2
+	   INR C
+
+L3:	   DCR E
+	   JNZ L1
+	   MOV A,B	// 1'S
+	   STA 2070
+	   MOV A,C	// 0'S
+	   STA 2071
+	   HLT
+
+L2:	   INR B
+	   JMP L3
+# ORG 2060H
+# DB 02
