e89fbfbad95e287b278deb8e8b63fd42c5ea2590
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2018-12-05 01:23:22 +0530

    progress

--- angular/angular-tour-of-heroes/src/app/app.module.ts
+++ angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- hero: Hero = {
-   id: 1,
-   name:'Windstorm'
- };
- constructor(){}
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
   ngOnInit() {
-    
   }
 
 }
-
