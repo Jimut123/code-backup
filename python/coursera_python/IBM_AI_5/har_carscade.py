1f6477d82cbbc59e7a73cbcfe792f2a73d06f03e
Jimut Bahan Pal <jimutbahanpal@yahoo.com>
2019-12-22 20:04:21 +0530

    Some more codes

---
+++ python/coursera_python/IBM_AI_5/har_carscade.py
@@ -0,0 +1,23 @@
+"https://d3c33hcgiwev3.cloudfront.net/imageAssetProxy.v1/veI59J6bEemCjApABi1-mA_82b6b5b5217459214d40fad5697d9682_face.jpg?expiry=1577059200000&hmac=CwJSgnfZrYwgRiTipb5wEpF9nbysEPBK49ZDN2hgYzA"
+
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+img = cv2.imread('face_1.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
